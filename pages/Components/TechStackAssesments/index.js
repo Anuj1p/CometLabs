58237b24b7434133b168b714ad5e72cef6147ceb
i_anujjangra <i_anujjangra@lidolearning.com>
2022-02-19 22:03:33 +0530

    Add Changes

--- pages/Components/TechStackAssesments/index.js
+++ pages/Components/TechStackAssesments/index.js
@@ -1,12 +1,42 @@
 import React from 'react'
-
-import cardData from '../../lib/CardData';
 import LockIcon from '@mui/icons-material/Lock';
 import Card from '../Card';
 
 import styles from './TechStackAssesments.module.css'
 
 const TechStackAssesments = () => {
+
+    const cardData = [
+        {
+            id: 1,
+            imgsrc: require('../../../images/react-icon.svg'),
+            heading: 'ReactJS 16.8++',
+            subHeading: '40 min',
+            buttontext: 'Start test'
+        }, 
+        {
+            id: 2,
+            imgsrc: require('../../../images/react-icon.svg'),
+            heading: 'Redux',
+            subHeading: '30 min',
+            buttontext: 'Start test'
+        }, 
+        {
+            id: 3,
+            imgsrc: require('../../../images/react-icon.svg'),
+            heading: 'ReactJS 16.8++',
+            subHeading: '40 min',
+            buttontext: 'Start test'
+        },
+        {
+            id: 4,
+            imgsrc: require('../../../images/react-icon.svg'),
+            heading: 'Redux',
+            subHeading: '30 min',
+            buttontext: 'Start test'
+        },
+    ];
+
     return (
         <div className={styles.techStackMainComponent} disabled="true">
             <div className={styles.techStackUpperContainer}>
